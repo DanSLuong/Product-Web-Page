e441981be84d3071607128808ef3124e11a46b01
DanSLuong <hayate58@gmail.com>
2018-06-22 13:26:26 -0500

    Update - Started working on the database template

--- database_setup.py
+++ database_setup.py
@@ -24,7 +24,10 @@ class Product(Base):
     name = Column(String(250), nullable=False)
     URL = Column(String(250), nullable=False)
     cost = Column(Integer, nullable=False)
-    reviews = Column(Integer, nullable=False)
+    reviews = Column(Integer)
+    stockCount = Column(Integer, nullable=False)
+    category = Column(String(250), nullable=False)
+
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User, backref='product')
 
@@ -38,9 +41,66 @@ class Product(Base):
             'URL': self.URL,
             'cost': self.cost,
             'reviews': self.reviews,
+            'stockCount': self.stockCount,
+            'category': self.category,
             'id': self.id,
         }
 
+
+#InventoryItems fields: InventoryItemId, Name, CurrentInventoryLevel, IdealInventoryLevel
+#InventoryChangeRecords fields: InventoryChangeId, InventoryItemId, Change (int)
+
+
+class Sale(Base):
+    __tablename__ = 'sale'
+
+    # SaleTransactionId
+    id = Column(Integer, primary_key=True)
+    dateTime = Column(date_trunc(text, timestamp), nullable=False)
+    totalSale = Column(Integer, nullable=False)
+    
+    team_id = Column(Integer, ForeignKey('team.id'))
+    team = relationship(Product, backref=backref('sale', cascade='all, delete'))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User, backref='items')
+
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializable format"""
+        return{
+            'id': self.id,
+            'dateTime': self.dateTime,
+            'totalSale': self.totalSale,
+        }
+
+
+class SaleItem(Base):
+    __tablename__ = 'saleItem'
+
+    id = Column(Integer, primary_key=True)
+    itemSold = Column(String(250), nullable=False)
+    quantity = Column(Integer, nullable=False)
+    
+    # SaleTransactionId
+    sale_id = Column(Integer, ForeignKey('sale.id'))
+    sale = relationship(Product, backref=backref('saleItem', cascade='all, delete'))
+    # ProductID
+    product_id = Column(Integer, ForeignKey('product.id'))
+    product = relationship(Product, backref=backref('saleItem', cascade='all, delete'))
+    
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User, backref='items')
+
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializable format"""
+        return{
+            'id': self.id,
+            'itemSold': self.itemSold,
+            'quantity': self.quantity,
+        }
 
 engine = create_engine('sqlite:///products.db')
 
---
+++ products.py
@@ -0,0 +1,47 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from database_setup import Base, Product, Sale, SaleItem, User
+
+engine = create_engine('sqlite:///products.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+# Create User
+User1 = User(name="Admin", email="hayate58@gmail.com",
+             picture='http://www.daviker.co.uk/wp-content/uploads/profile-blank.png')
+session.add(User1)
+session.commit()
+
+
+
+
+product1=Product(user_id=1, sku='754697343789', name='Blue Lash', URL='img/blue-lash-bluefront@2x.png', cost='23.00', stockCount = 100, category='Lashes')
+session.add(product1)
+
+product1=Product(user_id=1, sku='754697344366', name='Spark Lash', URL='img/spark-lash-sparkfront@2x.png', cost='22.00', stockCount = 100, category='Lashes')
+session.add(product1)
+
+product1=Product(user_id=1, sku='754697343857', name='Brown Lash', URL='img/brown-lash-brownfront@2x.png', cost='23.00', stockCount = 100, category='Lashes')
+session.add(product1)
+
+product1=Product(user_id=1, sku='754697344014', name='Original Mink Fiber', URL='img/originalminkfiber-originalfront@2x.png', cost='20.00', stockCount = 100, category='Lashes')
+session.add(product1)
+
+product1=Product(user_id=1, sku='754697344367', name='Premade Fans', URL='img/premade-fan-premadefans@2x.png', cost='18.00', stockCount = 100, category='Lashes')
+session.add(product1)
+
+product1=Product(user_id=1, sku='754697344367', name='Two tone purple lash', URL='img/two-tone-purple-lash-twotonepurplelashfront@2x.png', cost='23.00', stockCount = 100, category='Lashes')
+session.add(product1)
+
+product1=Product(user_id=1, sku='754697343864', name='Purple Lash', URL='img/purple-lash-purplefront@2x.png', cost='23.00', stockCount = 100, category='Lashes')
+session.add(product1)
+
+product1=Product(user_id=1, sku='754697344236', name='Pheonix Lash', URL='img/pheonix-phoenixlashfront 1@2x.png', cost='22.00', stockCount = 100, category='Lashes')
+session.add(product1)
+
+
+
+print "added products"
