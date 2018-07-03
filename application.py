e981f736fba6aabc345597be336aeecdd7cb906b
DanSLuong <hayate58@gmail.com>
2018-07-03 03:27:06 -0500

    Updated - Added new, edit, delete blog post. Cleaning up links

--- application.py
+++ application.py
@@ -1,14 +1,15 @@
 from __future__ import print_function
 from flask import (Flask,
-                    render_template,
-                    request, redirect,
-                    jsonify,
-                    url_for,
-                    flash,
-                    g)
+                   render_template,
+                   request, redirect,
+                   jsonify,
+                   url_for,
+                   flash,
+                   g)
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Blog, User#Product, Inventory, Sale, SaleItem, Employee
+# Product, Inventory, Sale, SaleItem, Employee
+from database_setup import Base, Blog, User
 from flask import session as login_session
 import squareconnect
 from squareconnect.rest import ApiException
@@ -49,23 +50,23 @@ except ApiException as e:
 # Pull product values
 value = 1
 squaredata = []
-while value<len(api_instances[0]):
-      if api_instances[0][value].item_data:
-            if api_instances[0][value].item_data.variations[0].item_variation_data.sku:
-                cost = float(
-                        api_instances[0][value].item_data.variations[0].item_variation_data.price_money.amount)
+while value < len(api_instances[0]):
+    if api_instances[0][value].item_data:
+        if api_instances[0][value].item_data.variations[0].item_variation_data.sku:
+            cost = float(
+                api_instances[0][value].item_data.variations[0].item_variation_data.price_money.amount)
 
-                squaredata.append([api_instances[0][value].item_data.name,
-                                  api_instances[0][value].item_data.description,
-                                  api_instances[0][value].item_data.category_id,
-                                  api_instances[0][value].item_data.product_type,
-                                  api_instances[0][value].item_data.variations[0].id,
-                                  api_instances[0][value].item_data.variations[0].item_variation_data.item_id,
-                                  api_instances[0][value].item_data.image_url,
-                                  api_instances[0][value].item_data.variations[0].item_variation_data.sku,
-                                  cost/100,
-                                  api_instances[0][value].item_data.variations[0].item_variation_data.inventory_alert_type])
-      value+=1
+            squaredata.append([api_instances[0][value].item_data.name,
+                               api_instances[0][value].item_data.description,
+                               api_instances[0][value].item_data.category_id,
+                               api_instances[0][value].item_data.product_type,
+                               api_instances[0][value].item_data.variations[0].id,
+                               api_instances[0][value].item_data.variations[0].item_variation_data.item_id,
+                               api_instances[0][value].item_data.image_url,
+                               api_instances[0][value].item_data.variations[0].item_variation_data.sku,
+                               cost/100,
+                               api_instances[0][value].item_data.variations[0].item_variation_data.inventory_alert_type])
+    value += 1
 """
 for i in range(len(squaredata)):
       #for j in range(len(squaredata[i])):
@@ -93,19 +94,27 @@ def showHome():
     return render_template('home.html')
 
 # Aboutus page
+
+
 @app.route('/aboutus')
 def showAboutUs():
     return render_template('aboutus.html')
 
 # Contact page
+
+
 @app.route('/contact')
 def showContact():
     return render_template('contactipad.html')
 
 # Product pages
+
+
 @app.route('/product/products')
 def showProducts():
     return render_template('product.html')
+
+
 """
 @app.route('/product/products')
 def showProducts():
@@ -119,45 +128,101 @@ def showProductInfo(team_id, player_id):
     return render_template('product.html', product=product)
 """
 
+
 @app.route('/product/shipping')
 def showShipping():
     return render_template('shipping.html')
 
 # Under the services tab.
+
+
 @app.route('/services/services')
 def showServices():
     return render_template('services.html')
 
+
 @app.route('/services/team')
 def showTeam():
     return render_template('team.html')
+
 
 @app.route('/services/clientcare')
 def showClientCare():
     return render_template('clientcare.html')
 
 # Course pages
+
+
 @app.route('/course_training/course')
 def showCourse():
     return render_template('coursemobile.html')
+
 
 @app.route('/course_training/success_stories')
 def showSuccessStories():
     return render_template('successstoriesipad.html')
 
-@app.route('/course_training/QandA')
-def showQandA():
+
+@app.route('/course_training/educational')
+def showEducational():
     return render_template('screen1.html')
+
+# Blog
+
 
 @app.route('/blog')
 def showBlog():
     blogs = session.query(Blog).all()
-    #return render_template('test.html', blogs=blogs)
     return render_template('blogipad.html', blogs=blogs)
+
+
+@app.route('/blog/new/', methods=['GET', 'POST'])
+def newBlogPost():
+    if request.method == 'POST':
+        newBlogPost = Blog(title=request.form['title'],
+                           dateValue=request.form['dateValue'],
+                           pictureURL=request.form['pictureURL'],
+                           story=request.form['story'])
+        session.add(newBlogPost)
+        session.commit()
+        return redirect(url_for('showBlog'))
+    else:
+        return render_template('newblogpost.html')
+
+
+@app.route('/blog/<int:blog_id>/edit/', methods=['GET', 'POST'])
+def editBlogPost(blog_id):
+    editBlogPost = session.query(Blog).filter_by(id=blog_id).one()
+    if request.method == 'POST':
+        if request.form['title']:
+            editBlogPost.title = request.form['title']
+        if request.form['dateValue']:
+            editBlogPost.dateValue = request.form['dateValue']
+        if request.form['pictureURL']:
+            editBlogPost.pictureURL = request.form['pictureURL']
+        if request.form['story']:
+            editBlogPost.pictureURL = request.form['story']
+        session.add(editBlogPost)
+        session.comment()
+        return redirect(url_for('showBlog'))
+    else:
+        return render_template('editblogpost.html', blog=editBlogPost)
+
+
+@app.route('/blog/<int:blog_id>/delete/', methods=['GET', 'POST'])
+def deleteBlogPost(blog_id):
+    blogPostToDelete = session.query(Blog).filter_by(id=blog_id).one()
+    if request.method == 'POST':
+        session.delete(blogPostToDelete)
+        session.commit()
+        return redirect(url_for('showBlog'))
+    else:
+        return render_template('deleteblogpost.html', blog=blogPostToDelete)
+
 
 @app.route('/test')
 def showTest():
-    products=[]
+    products = []
     for i in range(len(squaredata)):
         products.append(squaredata[i][0])
     return render_template('test2.html', products=products)
